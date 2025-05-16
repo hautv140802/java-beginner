9987852e00a2aa9ff8e521172a0885208951f6ac
hautv140802 <hautv1408@gmail.com>
2025-05-16 21:21:57 +0700

    Bai 5: Bien

---
+++ src/bai5_bien.java
@@ -0,0 +1,33 @@
+public class bai5_bien {
+    public static void main(String[] args) {
+        // Khai báo biến
+        int a;
+        byte tuoi;
+        float diemToan;
+
+        // Khai báo nhiều biến một lúc cùng kiểu
+        int c, d, e, f;
+
+
+        // khởi tạo biến
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 7.5;
+
+
+        System.out.println("Tuoi con sau trước gán giá trị mới: " + tuoiCon);
+
+        // tuoiCon sau khi gán thành 15
+        tuoiCon = 15;
+        System.out.println("Tuoi con sau khi gán giá trị mới: " + tuoiCon);
+
+        // hằng số
+
+        final int DOISO = 1000;
+
+
+
+
+
+    }
+}
