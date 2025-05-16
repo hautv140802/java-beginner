bf854620d87482103375df7bd9e65b0fe357c46a
hautv140802 <hautv1408@gmail.com>
2025-05-16 20:41:40 +0700

    Bai 3: commnents

---
+++ src/bai3_comments.java
@@ -0,0 +1,12 @@
+public class bai3_comments {
+    public static void main(String[] args) {
+    // comment in line with ctrl + /
+
+    // comment many lines with ctrl + shift + /
+      /*test
+        test 1*/
+
+
+    }
+
+}
