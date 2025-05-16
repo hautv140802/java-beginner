09d990296c927116135b69e941bbaa987370c916
hautv140802 <hautv1408@gmail.com>
2025-05-16 20:25:37 +0700

    Bai 2: Xuat du lieu

---
+++ src/bai2_xuatDuLieu.java
@@ -0,0 +1,25 @@
+public class bai2_xuatDuLieu {
+    public static void main(String[] args) {
+
+        // print break line
+        System.out.println("Dòng 1"); //sout
+        System.out.println("Dòng 2"); //sout
+        System.out.println("Dòng 3"); //ctrl + D -- duplicate line
+
+        // print in line
+        System.out.print("Dòng 4");
+        System.out.print("Dòng 5");
+
+        // \t
+        System.out.println("\t Trinh Van Hau");
+        System.out.println("\t Trinh Van Hau 1");
+
+        // \n
+        System.out.println("Trin Van Hau 2\nJava beginner ");
+
+        // Dấu nháy ""
+        System.out.println("Trin Van Hau \"Java beginner\"");
+
+
+    }
+}
