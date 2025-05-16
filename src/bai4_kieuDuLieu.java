8e72d6db567e5e0540a18b4e1e7dd7261bf776a0
hautv140802 <hautv1408@gmail.com>
2025-05-16 21:12:20 +0700

    Bai 4: Kiểu dữ liệu

---
+++ src/bai4_kieuDuLieu.java
@@ -0,0 +1,11 @@
+public class bai4_kieuDuLieu {
+    public static void main(String[] args) {
+        // kiểm tra kiểu nguyên
+        System.out.println("min kiểu int: " + Integer.MIN_VALUE);
+        System.out.println("max kiểu int: " + Integer.MAX_VALUE);
+
+        // kiểm tra kiểu byte
+        System.out.println("min kiểu byte: " + Byte.MIN_VALUE);
+        System.out.println("max kiểu byte: " + Byte.MAX_VALUE);
+    }
+}
