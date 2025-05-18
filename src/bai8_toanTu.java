3b945ec6c2a253beaef799ae3bb911dc961afb8c
hautv140802 <hautv1408@gmail.com>
2025-05-18 10:07:28 +0700

    Bai 8: Toán tử

---
+++ src/bai8_toanTu.java
@@ -0,0 +1,35 @@
+public class bai8_toanTu {
+    public static void main(String[] args) {
+        // toán tử cộng
+        int x = 8;
+        x+=5; // x=x+5
+        System.out.println("x= "+ x);
+
+        // toán tử trừ
+        int x2 = 8;
+        x2-=5; // x2=x2-5;
+        System.out.println("x2= " + x2);
+
+        // toán tử nhân
+        int x3 = 8;
+        x3*=5;
+        System.out.println("x3= " + x3);
+
+        // toán tử chia
+        double x4 = 8;
+        x4/=5;
+        System.out.println("x4= " + x4);
+
+
+        // so sánh
+        int a = 7;
+        int b = 9;
+        System.out.println("a==b " + (a==b));
+        System.out.println("a!=b " + (a!=b));
+        System.out.println("a>b " + (a>b));
+        System.out.println("a<b " + (a<b));
+        System.out.println("a<=b " + (a<=b));
+        System.out.println("a>=b " + (a>=b));
+
+    }
+}
