7a931bf75ab17343f829dc8cb3faa7a153911ec6
hautv140802 <hautv1408@gmail.com>
2025-05-18 09:41:02 +0700

    Bai 7: Các phép toán

---
+++ src/bai7_cacPhepToan.java
@@ -0,0 +1,28 @@
+public class bai7_cacPhepToan {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 5;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+
+        // phép công
+        System.out.println("a + b = " + (a+b));
+        // gán biến
+        int kq= a + b;
+        System.out.println("a + b = " + kq);
+
+        // phép trừ
+        System.out.println("a - b = " + (a-b));
+
+        // phép nhân
+        System.out.println("a * b = " + (a*b));
+
+        // phép chia
+        System.out.println("a / b = " + ((double) a/b));
+
+        // phép chia lấy dư
+        System.out.println("a % b = " + (a%b));
+
+
+    }
+}
